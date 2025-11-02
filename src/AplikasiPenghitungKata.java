d8e0cd44891c262818d841c4dd56677feb1341a3
tegar031 <tegar.adhitya31@gmail.com>
2025-11-02 12:38:24 +0800

    Update

--- src/AplikasiPenghitungKata.java
+++ src/AplikasiPenghitungKata.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
-/**
- *
- * @author User
- */
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
